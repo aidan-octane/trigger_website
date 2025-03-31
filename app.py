30c663dd109db3c604a99970d3d8439cb9dca603
aidan-octane <aidanaborden@gmail.com>
2025-03-30 20:14:10 -0400

    Updated

--- app.py
+++ app.py
@@ -1,22 +1,35 @@
-from flask import Flask, request, jsonify, render_template
-from flask_socketio import SocketIO, emit, join_room, leave_room
+from flask import Flask, request, jsonify, render_template, session
+from flask_socketio import SocketIO, emit
+from dotenv import load_dotenv
+load_dotenv()
+import os
+import requests
+import json
+from keys import endpoint
+headers = {'Content-type': 'application/json'}
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'FALLBACK_KEY')
 socketio = SocketIO(app, cors_allowed_origins="*")
+
 
 active_users = 0
 
-@socketio.on('connect')
-def handle_connect():
-    global active_users
-    active_users += 1
-    emit("user_count", active_users, broadcast=True)
 
-@socketio.on('disconnect')
-def handle_disconnect():
-    global active_users
-    active_users -= 1
-    emit("user_count", active_users, broadcast=True)
+
+# @socketio.on('connect')
+# def handle_connect():
+#     global active_users
+#     active_users += 1
+#     emit("user_count", active_users, broadcast=True)
+
+# @socketio.on('disconnect')
+# def handle_disconnect():
+#     global active_users
+#     active_users -= 1
+#     emit("user_count", active_users, broadcast=True)
+
 
 @app.route('/')
 def index():
@@ -25,7 +38,9 @@ def index():
 @app.route('/submit', methods=['POST'])
 def submit():
     data = request.json
-    return jsonify({"message": "Received!", "data": data})
+    response = requests.post(endpoint, data=data, headers=headers)
+    return jsonify({"message": "Received!", "data": response})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
